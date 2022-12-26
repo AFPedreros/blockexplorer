8b39739143b6e785d466686b52fef112fb8a0ad6
AFPedreros <felipe.pedreros94@gmail.com>
2022-12-26 18:59:12 -0500

    render tx of the block

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Alchemy, Network } from "alchemy-sdk";
+import { Alchemy, Network, Utils } from "alchemy-sdk";
 import { useEffect, useState } from "react";
 
 // Refer to the README doc for more information about using API
@@ -20,6 +20,8 @@ function App() {
     const [lastBlock, setLastBlock] = useState();
     const [blockNumber, setBlockNumber] = useState();
     const [transactions, setTransactions] = useState();
+    const [transactionsRows, setTransactionsRows] = useState();
+    const [toggle, setToggle] = useState(false);
 
     let formatted = 0;
 
@@ -44,14 +46,73 @@ function App() {
 
     function getPrevBlock() {
         setBlockNumber((block) => block - 1);
+        setToggle(false);
     }
 
     function getNextBlock() {
         setBlockNumber((block) => block + 1);
+        setToggle(false);
     }
 
     function seeBlockTransactions() {
-        console.log(transactions.transactions);
+        setToggle((prev) => !prev);
+
+        const blockTransactions = transactions.transactions;
+
+        const tenRows = blockTransactions.map((tx, index) => {
+            console.log(Utils.formatEther(tx.value.toString()));
+            const ethValue = parseInt(tx.value._hex, 16);
+            const rounded = Math.round(ethValue * 100) / 100000000000;
+            formatted = rounded.toFixed(2);
+
+            return (
+                <tr key={index} className=" border-b border-slate-300">
+                    <td className="px-4 py-2">
+                        {`${tx.hash.substring(0, 20)}...`}
+                    </td>
+                    <td onClick={seeBlockTransactions} className="px-4 py-2">
+                        {`${tx.from.substring(0, 20)}...`}
+                    </td>
+
+                    <td className="px-4 py-2 ">
+                        {`${tx.to.substring(0, 20)}...`}
+                    </td>
+                    <td className="px-4 py-2 ">{`${formatted} Ether`}</td>
+                </tr>
+            );
+        });
+
+        setTransactionsRows(() => {
+            return (
+                <div className="flex flex-col w-fit px-10">
+                    <table className=" text-left">
+                        <thead>
+                            <tr className=" bg-slate-100 border-b border-slate-300">
+                                <th className="px-4 py-2 "> Txn Hash</th>
+                                <th className="px-4 py-2 ">From</th>
+                                <th className="px-4 py-2 ">To</th>
+                                <th className="px-4 py-2 ">Value</th>
+                            </tr>
+                        </thead>
+                        <tbody>{tenRows}</tbody>
+                    </table>
+                    <div className="flex px-2 mx-auto w-full bg-slate-50 justify-between">
+                        <button onClick={getPrevBlock}>{"<"}</button>
+                        {lastBlock === blockNumber ? (
+                            <button
+                                className="text-slate-300"
+                                disabled={true}
+                                onClick={null}
+                            >
+                                {">"}
+                            </button>
+                        ) : (
+                            <button onClick={null}>{">"}</button>
+                        )}
+                    </div>
+                </div>
+            );
+        });
     }
 
     if (transactions) {
@@ -61,68 +122,72 @@ function App() {
     }
 
     return (
-        <div className="flex flex-col w-full  m-auto p-10">
-            <table className=" text-left w-full">
-                <thead>
-                    <tr className=" bg-slate-100 border-b border-slate-300">
-                        <th className="px-4 py-2 ">Block</th>
-                        <th className="px-4 py-2 ">Txn</th>
-                        <th className="px-4 py-2 ">Gas Used</th>
-                        <th className="px-4 py-2 ">Gas Limit</th>
-                        <th className="px-4 py-2 ">Base Fee</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr className=" border-b border-slate-300">
-                        <td className="px-4 py-2 cursor-pointer text-blue-400">
-                            {transactions ? blockNumber : "Loading"}
-                        </td>
-                        <td
-                            onClick={seeBlockTransactions}
-                            className="px-4 py-2 cursor-pointer text-blue-400"
-                        >
-                            {transactions
-                                ? transactions.transactions.length
-                                : "Loading"}
-                        </td>
+        <>
+            <div className="flex flex-col w-fit p-10">
+                <table className=" text-left w-full">
+                    <thead>
+                        <tr className=" bg-slate-100 border-b border-slate-300">
+                            <th className="px-4 py-2 ">Block</th>
+                            <th className="px-4 py-2 ">Txn</th>
+                            <th className="px-4 py-2 ">Gas Used</th>
+                            <th className="px-4 py-2 ">Gas Limit</th>
+                            <th className="px-4 py-2 ">Base Fee</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className=" border-b border-slate-300">
+                            <td className="px-4 py-2">
+                                {transactions ? blockNumber : "Loading"}
+                            </td>
+                            <td
+                                onClick={seeBlockTransactions}
+                                className="px-4 py-2 cursor-pointer text-blue-400"
+                            >
+                                {transactions
+                                    ? transactions.transactions.length
+                                    : "Loading"}
+                            </td>
 
-                        <td className="px-4 py-2 ">
-                            {transactions
-                                ? parseInt(
-                                      transactions.gasUsed._hex,
-                                      16
-                                  ).toLocaleString()
-                                : "Loading"}
-                        </td>
-                        <td className="px-4 py-2 ">
-                            {transactions
-                                ? parseInt(
-                                      transactions.gasLimit._hex,
-                                      16
-                                  ).toLocaleString()
-                                : "Loading"}
-                        </td>
-                        <td className="px-4 py-2 ">
-                            {transactions ? `${formatted} Gwei` : "Loading"}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <div className="flex px-2 mx-auto w-full bg-slate-50 justify-between">
-                <button onClick={getPrevBlock}>{"<"}</button>
-                {lastBlock === blockNumber ? (
-                    <button
-                        className="text-slate-300"
-                        disabled={true}
-                        onClick={getNextBlock}
-                    >
-                        {">"}
-                    </button>
-                ) : (
-                    <button onClick={getNextBlock}>{">"}</button>
-                )}
+                            <td className="px-4 py-2 ">
+                                {transactions
+                                    ? parseInt(
+                                          transactions.gasUsed._hex,
+                                          16
+                                      ).toLocaleString()
+                                    : "Loading"}
+                            </td>
+                            <td className="px-4 py-2 ">
+                                {transactions
+                                    ? parseInt(
+                                          transactions.gasLimit._hex,
+                                          16
+                                      ).toLocaleString()
+                                    : "Loading"}
+                            </td>
+                            <td className="px-4 py-2 ">
+                                {transactions ? `${formatted} Gwei` : "Loading"}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div className="flex px-2 mx-auto w-full bg-slate-50 justify-between">
+                    <button onClick={getPrevBlock}>{"<"}</button>
+                    {lastBlock === blockNumber ? (
+                        <button
+                            className="text-slate-300"
+                            disabled={true}
+                            onClick={getNextBlock}
+                        >
+                            {">"}
+                        </button>
+                    ) : (
+                        <button onClick={getNextBlock}>{">"}</button>
+                    )}
+                </div>
             </div>
-        </div>
+
+            {toggle && transactionsRows}
+        </>
     );
 }
 
